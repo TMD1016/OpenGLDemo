96ccb593893efdcc0a2b63f7ef5d964327f6e3ac
昊天斗罗 <1538529919@qq.com>
2024-06-29 16:17:50 +0800

    同步一下

--- OpenGL/shader/fragment.glsl
+++ OpenGL/shader/fragment.glsl
@@ -1,9 +1,42 @@
 
 
 
+//08_load_texture_exercise
+#version 330 core
+out vec4 FragColor;
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+uniform float factor;
+
+void main() {
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), abs(sin(factor * 0.2f)));
+    //FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2f);
+}
+//*/
+
+/* //07_load_texture
+#version 330 core
+in vec3 ourColor;
+in vec2 TexCoord;
+out vec4 FragColor;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    //FragColor = min(texture(texture1,TecCoord),texture(texture2,TexCoord));
+    //FragColor = texture(texture1,TexCoord);
+    FragColor = texture(texture2,TexCoord);
+}
+// */
 
 
- //06_glsl_exercise
+/* //06_glsl_exercise
 #version 330 core
 out vec4 FracColor;
 in vec3 ourColor;
--- OpenGL/shader/vertex.glsl
+++ OpenGL/shader/vertex.glsl
@@ -1,7 +1,45 @@
 
 
 
- //06_glsl_exercise
+//08_load_texture_exercise
+#version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec2 aTexCoord;
+
+out vec3 ourColor;
+out vec2 TexCoord;
+
+void main() {
+    gl_Position = vec4(aPos, 1.0f);
+    ourColor = aColor;
+    TexCoord = aTexCoord * 2.0f;
+}
+//*/
+
+
+
+/* //07_load_texture
+
+#version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec2 aTexCoord;
+
+out vec3 ourColor;
+out vec2 TexCoord;
+
+void main() {
+    gl_Position = vec4(aPos, 1.0f);
+    gl_PointSize = 10.0f;
+    ourColor = aColor;
+    TexCoord = aTexCoord * 2.0;
+}
+
+
+// */
+
+/* //06_glsl_exercise
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
