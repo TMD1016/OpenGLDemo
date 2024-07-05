d924c843c55f1572e4755447e85c46268cde5068
tm <taominha1016@gmail.com>
2024-07-05 10:13:07 +0800

    同步一下

--- OpenGL/shader/light_object_frag.glsl
+++ OpenGL/shader/light_object_frag.glsl
@@ -2,6 +2,39 @@
 
 
 
+/*// 23_direction_light
+#version 330 core
+out vec4 FragColor;
+in vec2 outTexCoord;
+
+void main() {
+    FragColor = vec4(1);
+}
+//*/
+
+
+/*// 22_light_map_exercise
+
+#version 330 core
+out vec4 FragColor;
+in vec2 outTexCoord;
+
+void main() {
+    FragColor = vec4(1);
+}
+
+//*/
+
+/*// 21_light_map
+#version 330 core
+out vec4 FragColor;
+in vec2 outTexCoord;
+
+void main() {
+    FragColor = vec4(1);
+}
+//*/
+
 /*//20_light_material
 #version 330 core
 out vec4 FragColor;
--- OpenGL/shader/light_object_vert.glsl
+++ OpenGL/shader/light_object_vert.glsl
@@ -1,5 +1,66 @@
 
 
+
+
+
+
+
+
+
+/*// 23_direction_light
+#version 330 core
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec3 Normal;
+layout(location = 2) in vec2 TexCoords;
+out vec2 outTexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main() {
+    gl_Position = projection * view * model * vec4(Position, 1.0f);
+    outTexCoord = TexCoords;
+}
+//*/
+
+
+/*// 22_light_map_exercise
+#version 330 core
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec3 Normal;
+layout(location = 2) in vec2 TexCoords;
+out vec2 outTexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main() {
+    gl_Position = projection * view * model * vec4(Position, 1.0f);
+    outTexCoord = TexCoords;
+}
+
+//*/
+
+
+/*// 21_light_map
+#version 330 core
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec3 Normal;
+layout(location = 2) in vec2 TexCoords;
+out vec2 outTexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main() {
+    gl_Position = projection * view * model * vec4(Position, 1.0f);
+    outTexCoord = TexCoords;
+}
+//*/
+
 /*//20_light_material
 #version 330 core
 layout(location = 0) in vec3 Position;
